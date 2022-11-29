facf19a134a4626b11ee9dcf941efdb745ba4bf3
Lívia Maria <liviamariasouza268@gmail.com>
2022-11-29 09:50:02 -0300

    video 5

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -3,22 +3,23 @@ import { CpfValidators } from '../validators/cpf-Validators';
 import { comparaValidators } from '../validators/compara-Validators';
 import { Usuario } from '../models/Usuário';
 import { StorageService } from '../services/storage.service';
-import{FormBuilder , FormGroup, Validators}from'@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.page.html',
   styleUrls: ['./registro.page.scss'],
 })
 export class RegistroPage implements OnInit {
-
   formRegistro: FormGroup;
-  Usuario:Usuario= new Usuario();
+  usuario: Usuario = new Usuario();
 
-mensagens={
   mensagens = {
     nome: [
       { tipo: 'required', mensagem: 'O campo Nome é obrigatório.' },
-      { tipo: 'minlength', mensagem: 'O nome deve ter pelo menos 3 caracteres.' },
+      {
+        tipo: 'minlength',
+        mensagem: 'O nome deve ter pelo menos 3 caracteres.',
+      },
     ],
     cpf: [
       { tipo: 'required', mensagem: 'O campo CPF é obrigatório.' },
@@ -30,46 +31,81 @@ mensagens={
     ],
     senha: [
       { tipo: 'required', mensagem: 'É obrigatório confirmar senha.' },
-      { tipo: 'minlength', mensagem: 'A senha deve ter pelo menos 6 caracteres.', },
-      { tipo: 'maxlength', mensagem: 'A senha deve ter no máximo 8 caractéres.' },
+      {
+        tipo: 'minlength',
+        mensagem: 'A senha deve ter pelo menos 6 caracteres.',
+      },
+      {
+        tipo: 'maxlength',
+        mensagem: 'A senha deve ter no máximo 8 caractéres.',
+      },
     ],
     confirmaSenha: [
       { tipo: 'required', mensagem: 'É obrigatório confirmar senha.' },
-      { tipo: 'minlength', mensagem: 'A senha deve ter pelo menos 6 caracteres.', },
-      { tipo: 'maxlength', mensagem: 'A senha deve ter no máximo 8 caractéres.' },
+      {
+        tipo: 'minlength',
+        mensagem: 'A senha deve ter pelo menos 6 caracteres.',
+      },
+      {
+        tipo: 'maxlength',
+        mensagem: 'A senha deve ter no máximo 8 caractéres.',
+      },
       { tipo: 'comparacao', mensagem: 'Deve ser igual a Senha.' },
     ],
-  },
+  };
 
-  constructor(private formBuilder: FormBuilder,private StorageService:StorageService) {
-    this.formRegistro=this.formBuilder.group({
-
-      nome:['',Validators.compose([Validators.required,Validators.minLength(3)])],
-      cpf:['',Validators.compose([Validators.required,CpfValidators.cpfValido])],
-      email:['',Validators.compose([Validators.required,Validators.email])],
-      senha:['',Validators.compose([Validators.required,Validators.minLength(6),Validators.maxLength(8)])],
-      confirmaSenha:['',Validators.compose([Validators.required,Validators.minLength(6),Validators.maxLength(8)])]
-
-},{
-  validator:comparaValidators('senha','confirmaSenha')
-})
+  constructor(
+    private formBuilder: FormBuilder,
+    private storageService: StorageService
+  ) {
+    this.formRegistro = this.formBuilder.group(
+      {
+        nome: [
+          '',
+          Validators.compose([Validators.required, Validators.minLength(3)]),
+        ],
+        cpf: [
+          '',
+          Validators.compose([Validators.required, CpfValidators.cpfValido]),
+        ],
+        email: [
+          '',
+          Validators.compose([Validators.required, Validators.email]),
+        ],
+        senha: [
+          '',
+          Validators.compose([
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(8),
+          ]),
+        ],
+        confirmaSenha: [
+          '',
+          Validators.compose([
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(8),
+          ]),
+        ],
+      },
+      {
+        validator: comparaValidators('senha', 'confirmaSenha'),
+      }
+    );
   }
 
-ngOnInit() {
-  }
+  ngOnInit(): void {}
 
-  async salvarRegistro(){
-    if(this.formRegistro.valid){
-      this.Usuario.nome=this.formRegistro.value.nome;
-      this.Usuario.cpf=this.formRegistro.value.cpf;
-      this.Usuario.email=this.formRegistro.value.email;
-      this.Usuario.senha=this.formRegistro.value.senha;
-      await this.StorageService .set(this.Usuario.email,this.Usuario)
-    }else{
-      alert('Formulário Inválido!')
-
+  async salvarRegistro() {
+    if (this.formRegistro.valid) {
+      this.usuario.nome = this.formRegistro.value.nome;
+      this.usuario.cpf = this.formRegistro.value.cpf;
+      this.usuario.email = this.formRegistro.value.email;
+      this.usuario.senha = this.formRegistro.value.senha;
+      await this.storageService.set(this.usuario.email, this.usuario);
+    } else {
+      alert('Formulário Inválido!');
     }
   }
-
-}
 }
